41f6edb0abed9df5748c185ee0a72e72f1e0a188
Samuel NJUNJI <njunjis14@gmail.com>
2025-09-08 18:25:58 +0300

    I have edited the input to request float varaibles.

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,6 @@
 ## Prompting for user feedback.
-monthlyIncome = int(input("Enter your monthly income:"))
-monthlyExpenses = int(input("Enter your total monthly expenses:"))
+monthlyIncome = float(input("Enter your monthly income:"))
+monthlyExpenses = float(input("Enter your total monthly expenses:"))
 
 ##Calculating monthly savings.
 monthlySavings=monthlyIncome-monthlyExpenses
@@ -11,6 +11,6 @@ rate = 0.05
 projectedSavings= monthlySavings * 12 + (monthlySavings*0.05)
 
 ##Output.
-print ("Your monthly savings are $",monthlySavings,".")
-print ("Projected saving after one year, with interest, is: $",projectedSavings,".")
+print ("Your monthly savings are $",monthlySavings)
+print ("Projected saving after one year, with interest, is: $",projectedSavings)
 
