2a7b4aaf0d7195e850ec867a595c9adef40b6b21
SNJUNJI <njunjis14@gmail.com>
2025-09-15 18:23:45 +0300

    Added a new file

---
+++ control_flow/task1.py
@@ -0,0 +1,18 @@
+noun =str(input("Hi, in order to create your madlib' I will need a few things from you. Please enter the requirements as requested."\n"please enter noun1")) 
+adjective = str(input("Noun 2"))
+noun3= str(intup("please enter noun 3"))
+noun4= str(input("Please enter noun4"))
+adjective1=str(input("please enter adjective 1"))
+adjective2=str(input("please enter adjective 2"))
+adjective3=str(input("Please enter adjective 3"))
+adjective4=str(input("Please enter adjective 4"))
+adjective5=str(input("Please enter adjective 5"))
+adjective6=str(input("Please enter adjective 6"))
+verb1=str(input("Please enter verb 1"))
+verb2=str(input("Please enter verb2, make sure it ends with '-ing'"))
+
+#Here's an interesting, short Mad Lib story with placeholders for verbs, adjectives, and nouns, designed to be flexible for use with if-else statements. The theme is a whimsical adventure in a magical forest:
+
+#One day, a [ADJECTIVE_1] [NOUN_1] decided to [VERB_1] through the [ADJECTIVE_2] forest. Suddenly, they stumbled upon a [ADJECTIVE_3] [NOUN_2] that was [VERB_ING_1] near a [NOUN_3]. The [NOUN_1] felt [ADJECTIVE_4] and chose to either [VERB_2] with the [NOUN_2] or [VERB_3] away to find a [ADJECTIVE_5] [NOUN_4]. Their choice led to a [ADJECTIVE_6] adventure that changed the forest forever!
+
+#This story has 6 adjectives, 4 nouns, 2 verbs, and 1 verb ending in -ing, with a decision point ("either [VERB_2] or [VERB_3]") that makes it easy to adapt with if-else statements for different outcomes. You can modify the story path based on user input or conditions in your code.
