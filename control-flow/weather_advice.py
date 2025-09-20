a1b9adf4118322c51c199ac8a8b196318254b6f4
Samuel NJUNJI <njunjis14@gmail.com>
2025-09-20 11:44:44 +0300

    Changed cold if statement

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -3,5 +3,5 @@ if weather=="sunny":
     print("Wear a t-shirt and sunglasses.")
 elif weather == "rainy":
     print("Don't forget your umbrella and a raincoat.")
-else:
+elif weather=="cold":
     print("Sorry, I don't have recommendations for this weather.")
