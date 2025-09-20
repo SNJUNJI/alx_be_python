e997c4324ef988a76b87092dfaf8bffc5838a976
Samuel NJUNJI <njunjis14@gmail.com>
2025-09-20 11:15:46 +0300

    Added weather_advice.py

---
+++ control_flow/weather_advice.py
@@ -0,0 +1,7 @@
+user_input = str(input("What's the weather like today? (sunny/rainy/cold):"))
+if user_input == "sunny":
+    print("Wera a t-shirt and sunglasses.")
+if user_input == "rainy":
+    print("Don't forget your umbrella and a raincoat.")
+if user_input == "cold":
+    print("Sorry, I don't have recommendations for this weather.")
