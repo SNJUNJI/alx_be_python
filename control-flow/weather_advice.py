d779ed0118dde6484b7d1dabcd41e5d0d8d96f11
Samuel NJUNJI <njunjis14@gmail.com>
2025-09-20 11:32:38 +0300

    Changed parameters again

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,5 +1,5 @@
 weather = str(input("What's the weather like today? (sunny/rainy/cold):"))
-if weather == "sunny":
+if weather=="sunny":
     print("Wera a t-shirt and sunglasses.")
 elif weather == "rainy":
     print("Don't forget your umbrella and a raincoat.")
