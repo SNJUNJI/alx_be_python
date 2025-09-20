664c29e649c4a4dd401bb623f3defd586662a998
Samuel NJUNJI <njunjis14@gmail.com>
2025-09-20 11:42:04 +0300

    Changed para again

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,6 +1,6 @@
 weather = str(input("What's the weather like today? (sunny/rainy/cold):"))
 if weather=="sunny":
-    print("Wera a t-shirt and sunglasses.")
+    print("Wear a t-shirt and sunglasses.")
 elif weather == "rainy":
     print("Don't forget your umbrella and a raincoat.")
 else:
