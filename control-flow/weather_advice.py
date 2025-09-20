291cd02c1629ef3a7dae5813fae9701fa767b2a7
Samuel NJUNJI <njunjis14@gmail.com>
2025-09-20 11:30:08 +0300

    Changed the user input place holder

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,7 +1,7 @@
-user_input = str(input("What's the weather like today? (sunny/rainy/cold):"))
-if user_input == "sunny":
+weather = str(input("What's the weather like today? (sunny/rainy/cold):"))
+if weather == "sunny":
     print("Wera a t-shirt and sunglasses.")
-elif user_input == "rainy":
+elif weather == "rainy":
     print("Don't forget your umbrella and a raincoat.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
