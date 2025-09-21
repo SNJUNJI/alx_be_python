fb2d1017b2f8caff8295f382f8c03a3184af3a23
Samuel NJUNJI <njunjis14@gmail.com>
2025-09-21 11:24:13 +0300

    n

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -2,9 +2,9 @@ Task=str(input("Enter your task: "))
 Priority=str(input("Priority (high/medium/low): "))
 Timebound=str(input("Is it time-bound?(yes/no): "))
 
-match priority:
-    case high if time_bound == "yes":
+match Priority:
+    case high if Timebound == "yes":
         print (f'Reminder: "{Task}" is a high priority task that requires immediate attention today!')
     
-    case low if time_bound == "no":
+    case low if Timebound == "no":
         print(f'Reminder: "{Task}" is a low priority task. Consider completing it when you have free time.')
