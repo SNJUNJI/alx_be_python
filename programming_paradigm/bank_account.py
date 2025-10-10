1165dcbe89546cf879ba28e8dd89651670cd6e3e
Samuel NJUNJI <njunjis14@gmail.com>
2025-10-10 11:39:30 +0300

    Added new file

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -10,4 +10,4 @@ class BankAccount:
             return True
         return False
     def display_balance(self):
-        print(f"Account Balance : $ {self.account_balance}")
+        print(f"Current Balance: {self.account_balance}")
