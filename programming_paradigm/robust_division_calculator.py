c1194b2989c6410b120483b93befa7ef080afed0
Samuel NJUNJI <njunjis14@gmail.com>
2025-10-10 12:14:53 +0300

    NF

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -3,10 +3,10 @@ def safe_divide(numerator, denominator):
         num=float(numerator)
         den=float(denominator)
     except  ValueError:
-        return print("Error: Non-numeric input provided.")
+        return "Error: Non-numeric input provided."
     
     try:
         result=num/den
         return result
     except ZeroDivisionError:
-        return print("Error: Division by zero.")
+        return "Error: Division by zero."
