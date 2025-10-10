8682b23815b4453dd6089642c4e95dd4cd78c9a7
Samuel NJUNJI <njunjis14@gmail.com>
2025-10-10 12:16:40 +0300

    NF

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -3,7 +3,7 @@ def safe_divide(numerator, denominator):
         num=float(numerator)
         den=float(denominator)
     except  ValueError:
-        return "Error: Non-numeric input provided."
+        return "Error: Please enter numeric values only."
     
     try:
         result=num/den
